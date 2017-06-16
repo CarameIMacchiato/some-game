7e1587a1d2ccf435619c6ed27aad58e5a7504a9c
Carmelito Gutierrez <carmelitogutierrez@gmail.com>
2017-06-15 20:30:40 -0700

    Add blocks, blocks falling, and spawning

---
+++ Assets/Scripts/BlockBehavior.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehavior : MonoBehaviour {
+
+	float fall = 0;
+	public float fallSpeed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckUserInput();
+
+	}
+
+	//Assign controls to arrow keys
+	void CheckUserInput() {
+		
+		if (Input.GetKeyDown(KeyCode.RightArrow)) {
+			transform.position += new Vector3(1, 0, 0);
+
+			if (CheckIsValidPosition()) {
+
+			} else {
+				transform.position += new Vector3(-1, 0, 0);
+			}
+		}
+	    else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+			transform.position += new Vector3(-1, 0, 0);
+			
+			if (CheckIsValidPosition()) {
+
+			} else {
+				transform.position += new Vector3(1, 0, 0);
+			}
+		}
+         else if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			transform.Rotate(0, 0, 90);
+
+			if (CheckIsValidPosition()) {
+				
+			} else {
+				transform.Rotate(0, 0, -90);
+			}
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - fall >= fallSpeed) {
+			transform.position += new Vector3(0, -1, 0);
+
+			if (CheckIsValidPosition()) {
+
+			} else {
+				transform.position += new Vector3(0, 1, 0);
+
+				enabled = false;
+
+				FindObjectOfType<Game>().spawnBlocks();
+			}
+
+			fall = Time.time;
+		}
+	}
+
+	bool CheckIsValidPosition() {
+
+		foreach (Transform mino in transform) 	{
+
+			Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+
+			if (FindObjectOfType<Game>().CheckIsInsideGrid(pos) == false) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	public static int gridWidth = 10;
+	public static int gridHeight = 11;
+
+	public GameObject[] blocks;
+
+	// Use this for initialization
+	void Start () {
+		spawnBlocks();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public bool CheckIsInsideGrid(Vector2 pos) {
+		return ((int)pos.x >= 0 && (int)pos.x < gridWidth && (int)pos.y >= 0);
+	}
+
+	public Vector2 Round(Vector2 pos) {
+		return new Vector2(Mathf.Round(pos.x), Mathf.Round(pos.y));
+	}
+
+	public void spawnBlocks() {
+		GameObject element = blocks[Random.Range(0, blocks.Length)];
+		Vector2 spawnPos = new Vector2(5.0f, 11f);
+		Quaternion spawnRot = Quaternion.identity;
+		GameObject SpawnObject = (GameObject)Instantiate(element, spawnPos, spawnRot);
+	}
+}
